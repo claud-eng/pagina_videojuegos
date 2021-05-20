329050b25d32d424a554ec2c883a7a733dbd301f
Marcelo Silva <grimlock31.2002@gmail.com>
2021-05-20 19:53:17 -0400

    Actualizacion version 5.1

--- docs/js/validador.js
+++ docs/js/validador.js
@@ -20,3 +20,75 @@ $("#formulario1").validate({
         }
     } 
 });
+
+jQuery(document).ready(function () {
+    // Show password Button
+    $("#formulario2").validate({
+        rules: {
+            "txtNombre": {
+                required: true,
+                minlength: 3
+            },
+            "txtApellido": {
+                required: true,
+                minlength: 3
+            },
+            "txtFechaNacimiento": {
+                required: true
+            },
+            "cmbRegion": {
+                required: true
+            },
+            "txtEmail": {
+                required: true,
+                email: true
+            },
+            "txtContrasena": {
+                required: true,
+                minlength: 5
+            },
+            "txtRepetirContrasena": {
+                required: true,
+                equalTo: '#id_txtContrasena'
+            },
+            "novedades": {
+                required: true,
+            }
+    
+        }, // --> Fin de reglas
+        messages: {
+            "txtNombre": {
+                required: 'Por favor, ingrese su nombre.',
+                minlength: 'Min. 3 caract.'
+            },
+            "txtApellido": {
+                required: 'Por favor, ingrese su apellido.',
+                minlength: 'Min. 3 caract.'
+            },
+            "txtFechaNacimiento": {
+                required: 'Por favor, seleccione su fecha de nacimiento.'
+            },
+            "cmbRegion": {
+                required: 'Por favor, seleccione una región.'
+            },
+            "txtEmail": {
+                required: 'Por favor, ingrese su email.',
+                email: 'No cumple formato.'
+            },
+            "txtContrasena": {
+                required: 'Por favor, ingrese una contraseña.',
+                minlength: 'Min. 5 caract.'
+            },
+            "txtRepetirContrasena": {
+                required: 'Por favor, debe repetir la contraseña anterior.',
+                equalTo: 'Las contraseñas no coinciden, intente de nuevo.'
+            },
+            "novedades": {
+                required: 'Por favor, seleccione una opción'
+            }
+        } //-->Fin de mensajes
+     
+    });
+    
+ 
+});
--- docs/js/verPassword.js
+++ docs/js/verPassword.js
@@ -15,3 +15,27 @@ jQuery(document).ready(function () {
 
     });
 });
+
+jQuery(document).ready(function () {
+    // Show password Button
+    $("#showpassword").on('click', function () {
+ 
+        var pass = $("#id_txtContrasena2");
+        var pass2 = $("#id_txtRepetirContrasena2");
+ 
+        var fieldtype = pass.attr('type');
+        var fieldtype2 = pass2.attr('type');
+ 
+        if (fieldtype == 'password' & fieldtype2 == 'password') {
+            pass.attr('type', 'text');
+            pass2.attr('type', 'text');
+            $(this).text("Ocultar Password");
+        } else {
+            pass.attr('type', 'password');
+            pass2.attr('type', 'password');
+            $(this).text("Ver Password");
+        }
+ 
+    });
+ 
+});
