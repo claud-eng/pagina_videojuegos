b3245f18854943fa214f3145175224300cd4b975
Gabriel Jiménez <gabrieljp2001@gmail.com>
2021-07-08 18:21:55 -0400

    Actualizacion version 8.2 Token login y css

--- dalepausa/dalepausa/urls.py
+++ dalepausa/dalepausa/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     # Login and Logout
     path('login/', LoginView.as_view(redirect_authenticated_user=True,template_name='Usuario/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='Usuario/logout.html'), name='logout'),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='Usuario/index.html'), name='home'),
 
     # localhost:8000/api-token-auth
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  #token
